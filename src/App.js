fd21e37aa2f47930d6c7392695e4207a0cd6745a
benbenfan <byfan@wisc.edu>
2019-07-16 01:03:33 -0500

    added herokuapp and created basic pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Player from './Player.js'
 
 function App() {
   return (
@@ -8,15 +9,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click above or below to login!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="./Player"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login
         </a>
       </header>
     </div>
